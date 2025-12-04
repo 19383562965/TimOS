b62f22f7ada5e76e4f299c6e3e28a116e6bc7aa9
Timurs Koleda <timurs.koleda@gmail.com>
2025-12-04 19:48:44 +0100

    Update settings.js

--- apps/settings/settings.js
+++ apps/settings/settings.js
@@ -4,11 +4,12 @@ const dock = document.getElementById("dock");
 
 fullWidthTaskbarBtn.addEventListener("click", () => {
   dock.style.transition = "0.2s";
-  dock.style.minWidth = "100%";
-  dock.style.bottom = "0";
+  dock.style.minWidth = "700px";
+  dock.style.bottom = "5px";
   dock.style.borderRadius = "0px";
   dock.style.padding = "10px 0";
-  dock.style.border = "none";
+  dock.style.border = "solid 0px rgba(0, 0, 0, 0.15)";
+  dock.style.backgroundFilter = "blur(1000px)";
 });
 
 floatingTaskbarBtn.addEventListener("click", () => {
