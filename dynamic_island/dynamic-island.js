23008cc1b4c5ad00131dd86de3ff38b12ebe66f0
Timurs Koleda <timurs.koleda@gmail.com>
2025-12-10 20:07:05 +0100

    Update dynamic-island.js

---
+++ dynamic_island/dynamic-island.js
@@ -0,0 +1,33 @@
+dynamicIsland = document.getElementById("dynamic-island");
+dynamicIsland.style.height = "100px";
+dynamicIsland.style.width = "100px";
+dynamicIsland.innerHTML = `<ion-icon name="hourglass-outline" style="color: orange;"></ion-icon>`;
+dynamicIsland.style.fontSize = "60px";
+dynamicIsland.style.top = "10px";
+
+window.onload = (event) => {
+  dynamicIsland.style.height = "26px";
+  dynamicIsland.style.width = "96px";
+  dynamicIsland.innerHTML = ``
+  dynamicIsland.style.fontSize = "26px";
+  dynamicIsland.style.top = "0";
+};
+
+const photosIcon = document.getElementById("photos");
+
+photosIcon.onclick = function () {
+  setTimeout(myTimer, 1000);
+  dynamicIsland.style.height = "100px";
+  dynamicIsland.style.width = "100px";
+  dynamicIsland.innerHTML = `<ion-icon name="checkmark-outline" style="color: green;"></ion-icon>`;
+  dynamicIsland.style.fontSize = "60px";
+  dynamicIsland.style.top = "10px";
+  function myTimer() {
+    dynamicIsland.style.height = "26px";
+    dynamicIsland.style.width = "96px";
+    dynamicIsland.innerHTML = ``
+    dynamicIsland.style.fontSize = "26px";
+    dynamicIsland.style.top = "0";
+    clearTimeout(myTimer);
+  }
+}
